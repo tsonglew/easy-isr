6424fc528b37875a19ccbe9de94933064b4941f7
klew <417879751@qq.com>
2019-07-25 20:45:12 +0800

    add resize func

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import os
 import argparse
 
-from utils import handle_dir, load_model
+from utils import handle_dir, load_model, resize_pics
 
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--input_dir', required=False, default=os.path.join(os.path.abspath('.'), 'test_inputs'),  help='')
-parser.add_argument('--output_dir', required=False, default=os.path.join(os.path.abspath('.'), 'test_outputs'), help='')
+parser.add_argument('--input_dir', required=False, default=os.path.join(os.path.abspath('.'), 'test_inputs'))
+parser.add_argument('--output_dir', required=False, default=os.path.join(os.path.abspath('.'), 'test_outputs'))
+parser.add_argument('--mode', required=False, default='isr')
 parser.add_argument('--weights', required=False, default=os.path.join(
     os.path.abspath('.'), 
     'weights',
     'rdn-C6-D20-G64-G064-x2', 
     'ArtefactCancelling', 
     'rdn-C6-D20-G64-G064-x2_ArtefactCancelling_epoch219.hdf5'
-    ), help='')
-parser.add_argument('--width', required=False, type=int, default=960, help='')
-parser.add_argument('--height', required=False, type=int, default=540, help='')
-parser.add_argument('--fixed_proportion', required=False, type=bool, default=True, help='')
+    ))
+parser.add_argument('--width', required=False, type=int, default=1920)
+parser.add_argument('--height', required=False, type=int, default=1080)
+parser.add_argument('--resize', required=False, type=bool)
+parser.add_argument('--fixed_proportion', required=False, type=bool)
+parser.add_argument('--padding_size', required=False, type=int, default=8)
+parser.add_argument('--by_patch_of_size', required=False, type=int, default=None)
 
 
 if __name__ == '__main__':
-    args = parser.parse_args()
-    rdn = load_model(args.weights)
-    handle_dir(args.input_dir, args.output_dir, rdn, args.width, args.height, args.fixed_proportion)
+    print(args.fixed_proportion)
+    if args.mode == 'isr':
+        rdn = load_model(args.weights)
+        handle_dir(args.input_dir, args.output_dir, rdn, args.width, args.height, args.resize, args.fixed_proportion, args.padding_size, args.by_patch_of_size)
+    elif args.mode == 'resize':
+        resize_pics(args.input_dir, (args.height, args.width), args.fixed_proportion)
+
     
--- utils.py
+++ utils.py
@@ -7,11 +7,23 @@ from tqdm import tqdm
 from PIL import Image
 from ISR.models import RDN
 
+def resize_pics(dir, shape, fp):
+    for root, dirs, files in os.walk(dir):
+        for f in files:
+            file_path = os.path.join(root, f)
+            img = cv2.imread(file_path)
+            if img.shape[:2] == shape:
+                print(file_path, " shape: ", img.shape, " skipped.")
+                continue
+            resized_img = im_resize(img, shape, fp)
+            print(file_path, " shape: ", img.shape, " => ", shape)
+            cv2.imwrite(file_path, resized_img)
 
 def im_resize(img, shape, fixed_proportion):
-    if fixed_proportion:
+    if fixed_proportion is not None:
+        # opencv-python resize: (image, (width, height))
         return cv2.resize(img, (shape[1], shape[1] * img.shape[0] // img.shape[1]))
-    return cv2.resize(img, (shape[1], shape[1]))
+    return cv2.resize(img, (shape[1], shape[0]))
 
 def resize_np_array(np_array, width, height):
     np.resize(np_array, (width, height))
@@ -31,17 +43,35 @@ def load_model(model_path):
     rdn.model.load_weights(model_path)
     return rdn
 
-def handle_dir(src_dir, tgt_dir, rdn, width, height, fp):
+def get_all_handled_imgs(tgt_path):
+    return set(os.listdir(tgt_path))
+
+def handle_dir(src_dir, tgt_dir, rdn, width, height, rs, fp, ps, bpos):
     filenames = os.listdir(src_dir)
+    handled_imgs = get_all_handled_imgs(tgt_dir)
+    error_imgs = []
 
-    for i in filenames:
-        handle_file(os.path.join(src_dir, i), os.path.join(tgt_dir, i), rdn, width, height, fp)
+    pbar = tqdm(filenames)
+    for f in pbar:
+        if f in handled_imgs:
+            continue
+        try:
+            pbar.set_description("Processing %s" % f)
+            handle_file(os.path.join(src_dir, f), os.path.join(tgt_dir, f), rdn, width, height, rs, fp, ps, bpos)
+        except Exception:
+            error_imgs.append(f)
+        break
 
-def handle_file(src_path, tgt_path, rdn, width, height, fp):
-    print('Handle file: ', src_path)
+    print("Error files: ", error_imgs)
+
+
+def handle_file(src_path, tgt_path, rdn, width, height, rs, fp, ps, bpos):
     img = cv2.imread(src_path)
     lr_img = np.array(img)
-    sr_img = rdn.predict(lr_img)
-    resized_img = im_resize(sr_img, (height, width), fp)
+    sr_img = rdn.predict(lr_img, padding_size=ps, by_patch_of_size=bpos)
+    while sr_img.shape[1] < width:
+        sr_img = rdn.predict(sr_img, padding_size=ps, by_patch_of_size=bpos)
+    if rs:
+        resized_img = im_resize(sr_img, (height, width), fp)
+    resized_img = sr_img
     cv2.imwrite(tgt_path, resized_img)
-    print('Saved to: ', tgt_path)
